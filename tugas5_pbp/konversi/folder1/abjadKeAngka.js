b91252915c2832ba280da8e024b5c2a778ec318f
nandsh26 <nandasawaliah001@gmail.com>
2025-10-20 21:02:04 +0700

    Refactor abjadKeAngka function and add export

--- tugas5_pbp/konversi/folder1/abjadKeAngka.js
+++ tugas5_pbp/konversi/folder1/abjadKeAngka.js
@@ -1,11 +1,10 @@
 //untuk mengonversi abjad ke angka
-
 function abjadKeAngka(teks) { 
   teks = teks.toUpperCase();
   const hasil = [];
   for (let char of teks) {
     if (char >= 'A' && char <= 'Z') {
-      hasil.push(char.charCodeAt(0) - 64); // A=1, B=2, dst
+      hasil.push(char.charCodeAt(0) - 64); 
     }
   }
   return hasil;
